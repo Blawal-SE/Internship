f06526214b8c9af2ff5e7106159e7ed976e4c80e
HAIER-PC\Hamza Aslam <blawal761@gmail.com>
2020-10-01 18:50:48 +0500

    Api CRUd Done Through Ajax

--- jquery/Script/StudentManagerWithJquery.js
+++ jquery/Script/StudentManagerWithJquery.js
@@ -1,48 +1,38 @@
-function addStudent(isruntime) {
+var studentArray=[];
+var baseUrl='https://localhost:44330/api/Student';
+function addStudent() {
    
     var name = $('#Name').val(); //document.getElementById("Name").value;
     var fName = $('#FName').val(); // document.getElementById("FName").value;
     var email = $('#email').val(); //document.getElementById("FName").value;
     var phone = $('#phone').val(); //document.getElementById("phone").value;
     var password = $('#Password').val(); //document.getElementById("Password").value;
-    if (isruntime) {
-        var confirmPassword = password;
-
-    }
-    //document.getElementById("ConfrimPassword").value;
-    var dob = $('#DOB').val(); //document.getElementById("DOB").value;
+ 
+    var confirmPassword= $('#ConfrimPassword').val(); //document.getElementById("ConfrimPassword").value;
+    var dob = $('#DOB').val().toString(); //document.getElementById("DOB").value;
 
     if (validateStudentForm(name, fName, email, phone, password, confirmPassword)) {
+  
+  var student = {
+   
+    "Name": name,
+    "FName": fName,
+    "Email": email,
+    "Phone": phone,
+    "Dob": dob.toString(),
+     "Password": password,
+     "ConfirmPassword": confirmPassword
+ }
+   debugger;
+ var url='https://localhost:44330/api/Student';
+ $.post(url,student,
+    function(data,status){
+        debugger;
+ 
+});
 
-        var stdArr = [];
-        //var stdDobStr = stdDob.toString();
-        var student = {
-            "Id": 1,
-            "name": name,
-            "fName": fName,
-            "email": email,
-            "Phone": phone,
-            "dob": dob.toString(),
-            "password": password,
-            "ConfrimPassword": confirmPassword
-        }
-        var getStdArr = JSON.parse(localStorage.getItem("student"));
-
-        if (getStdArr != null) {
-            var lastStudent = getStdArr[getStdArr.length - 1];
-
-            stdArr = getStdArr;
-            student.Id = lastStudent.Id + 1;
-        }
-        stdArr.push(student);
-        localStorage.setItem("student", JSON.stringify(stdArr));
-        if (isruntime) {
-
-            studentList();
-
-        }
-    } else {
-        return false;
+     } else {
+         return false;
     }
 }
 
@@ -127,86 +117,138 @@ function addStudentDynamic(student) {
 
 
 function studentList() {
-
-    var stdList = JSON.parse(localStorage.getItem("student"));
-    if (stdList == null) {
-        return false;
-
-    }
-
-    // var tBody = $('#stdList').val();//document.getElementById("stdList");
+  
+  // var stdList =GetStudentListApi;//JSON.parse(localStorage.getItem("student"));
+  
+   debugger;
+   // ajax start to get all student and put records in Table
+   $.ajax({
+    Method: 'GET',
+    url: baseUrl,
+    contentType: 'text/application/json; charset=UTF-8',  //send type of data to sever
+    // dataType: 'text/json', //retrun type of data from server
+   traditional: true,
+   
+   success: function (response, statusText, xhr) {
+      
     var appendRow = "";
-    stdList.forEach(function(value, index) {
+    response.forEach(function(value, index) {
        
-        appendRow += "<tr><td>" + value.name + "</td>" + "<td>" + value.fName + "</td>" + "<td>" + value.email + "</td>" + "<td>" + value.Phone + "</td>" + "<td>" + value.dob + "</td>" + "<td>" + value.password + "</td>";
+        appendRow += "<tr><td>" + value.Name + "</td>" + "<td>" + value.FName + "</td>" + "<td>" + value.Email + "</td>" + "<td>" + value.Phone + "</td>" + "<td>" + value.Dob + "</td>" + "<td>" + value.Password + "</td>";
         appendRow += "<td><button class='editBtn'  data-id=" + value.Id + ">Edit</button>";
         appendRow += "<button class='deleteBtn'  data-id=" + value.Id + ">Delete</a></td></tr>";
         //tBody.innerHTML += appendRow;
 
     });
     $('#stdList').html('').append(appendRow);
-  
-
+    
     $('.deleteBtn').click(function() {
-       
-        var id = parseInt($(this).attr('data-id'));
-        deleteStudent(id);
-    });
-    $('.editBtn').click(function() {
-        var id = parseInt($(this).attr('data-id'));
-        var parent = $(this).closest('tr');
+        debugger;
+         var id = parseInt($(this).attr('data-id'));
+         var url=baseUrl+'?id='+id;
+    $.delete(url,function(response,status){
+      studentList();
+     });
+        // deleteStudent(id);
+     });
+     $('.editBtn').click(function() {
         
-        var student=getStudent(id);
-        var row = "<td>  <input id='Name' type='text'  value='"+student.name+"'/></td>";
-        row += "<td>  <input id='FName' type='text'  value='"+student.fName+"'/></td>";
-        row += "<td>  <input id='email' type='text'  value='"+student.email+"'/></td>";
-        row += "<td>  <input id='phone' type='text'  value='"+student.Phone+"'/></td>";
-        row += "<td>  <input id='DOB' type='date' value='"+student.dob+"'/></td>";
-        row += "<td>  <input id='Password' type='password' value='"+student.password+"'/></td>";
-
-        row += "<td><button id='updateBtn' type='button'>Update</button><button id='updateCancelBtn' type='button'>Cancel</button>  </td>";
-        parent.html('').append(row);
-       
-        var row2 = "<tr><td> <label id='Namelb'/></td>";
-        row2 += "<td><td>";
-        row2 += "<td><label id='emaillb'></label></td>";
-        row2 += "<td></td>";
-        row2 += "<td></td>";
-        row2 += "<td><label id='Passwordlb'></label></td>";
-        row2 += "<td></td>";
-        $(row2).insertAfter(parent);
-        $('#updateBtn').click(function(){
+         var id = parseInt($(this).attr('data-id'));
+         var url=baseUrl+'?id='+id;
+         var parent = $(this).closest('tr');
+         $.get(url,function(response,status){
+            debugger;
+            
+         var student=response;
+         var row = "<td>  <input id='Name' type='text'  value='"+student.Name+"'/></td>";
+         row += "<td>  <input id='FName' type='text'  value='"+student.FName+"'/></td>";
+         row += "<td>  <input id='email' type='text'  value='"+student.Email+"'/></td>";
+         row += "<td>  <input id='phone' type='text'  value='"+student.Phone+"'/></td>";
+         row += "<td>  <input id='DOB' type='date' value='"+student.Dob+"'/></td>";
+         row += "<td>  <input id='Password' type='password' value='"+student.Password+"'/></td>";
+ 
+         row += "<td><button id='updateBtn' type='button'>Update</button><button id='updateCancelBtn' type='button'>Cancel</button>  </td>";
+         parent.html('').append(row);
         
+         var row2 = "<tr><td> <label id='Namelb'/></td>";
+         row2 += "<td><td>";
+         row2 += "<td><label id='emaillb'></label></td>";
+         row2 += "<td></td>";
+         row2 += "<td></td>";
+         row2 += "<td><label id='Passwordlb'></label></td>";
+         row2 += "<td></td>";
+         $(row2).insertAfter(parent);
+         
+         $('#updateBtn').click(function(){
+         
             var parent=$(this).closest('tr');
-           var student= getStudent(id);
+           var student= {};//getStudent(id);
            student.Id=id;
-           student.name=parent.find('#Name').val();
-           student.fName=parent.find('#FName').val();
-           student.email=parent.find('#email').val();
+           student.Name=parent.find('#Name').val();
+           student.FName=parent.find('#FName').val();
+           student.Email=parent.find('#email').val();
            student.Phone=parent.find('#phone').val();
-           student.dob=parent.find('#DOB').val();
-           student.password=parent.find('#Password').val();
-           student.confirmPassword=parent.find('#Password').val();
+           student.Dob=parent.find('#DOB').val();
+           student.Password=parent.find('#Password').val();
+           student.ConfirmPassword=parent.find('#Password').val();
             if(validateStudent(student,parent.next()))
-           {
-           var students=JSON.parse(localStorage.getItem("student"));
-           students.forEach(function(value, index) {
-            if (value.Id == student.Id) {
-                
-                students[index] = student;
-                localStorage.setItem("student", JSON.stringify(students));
-                studentList();
-                 }
-             });
-           }
+           { debugger;
+                //var url='https://localhost:44330/api/Student';
+             $.ajax({
+                     type: 'PUT',
+                     async: false,  
+                     url: baseUrl,
+                     //contentType: 'application/json; charset=UTF-8',  //send type of data to sever
+                    // traditional: true,
+                     data:{
+                         Id:student.Id,
+                         Name: student.Name,
+                         Fname: student.FName,
+                         Email: student.Email,
+                         Phone: student.Phone,
+                         Dob: student.Dob,
+                         Password: student.Password,
+                         ConfirmPassword: student.ConfirmPassword
+
+                     },
+                     success: function (response, statusText, xhr) {
+                        studentList();
+         
+                     },
+                     error: function (XMLHttpRequest, textStatus, errorThrown) {                       
+                         alert(XMLHttpRequest.responseText);
+         
+                     }
+                  });
+           
+              
+                 
+             }
+           
         });
     
         $('#updateCancelBtn').click(function(){
             studentList();
         });
 
+           });
+        
+ 
+     });
+ 
 
-    });
+   },
+   error: function (XMLHttpRequest, textStatus, errorThrown) {                       
+       alert(XMLHttpRequest.responseText);
+
+   }
+});
+   //ajax end 
+
+
+    // var tBody = $('#stdList').val();//document.getElementById("stdList");
+  
+  
 
 }           
 
@@ -235,16 +277,19 @@ function addNewStudentRow() {
         var parent=$(this).closest('tr');
         var student= {}
        
-        student.name=parent.find('#Name').val();
-        student.fName=parent.find('#FName').val();
-        student.email=parent.find('#email').val();
+        student.Name=parent.find('#Name').val();
+        student.FName=parent.find('#FName').val();
+        student.Email=parent.find('#email').val();
         student.Phone=parent.find('#phone').val();
-        student.dob=parent.find('#DOB').val();
-        student.password=parent.find('#Password').val();
-        student.confirmPassword=parent.find('#Password').val();
+        student.Dob=parent.find('#DOB').val();
+        student.Password=parent.find('#Password').val();
+        student.ConfirmPassword=parent.find('#Password').val();
          if(validateStudent(student,parent.next()))
         {
-            addStudentDynamic(student);
+            $.post(baseUrl,student,function(response,status){
+              studentList();
+            });
+           // addStudentDynamic(student);
         }
     });
     $('#removeBtn').click(function() {
@@ -253,35 +298,35 @@ function addNewStudentRow() {
     });
 
 }
-//commen
+
 
 function validateStudent(student,parent) {
   
-debugger;
+
     var check = true;
     //straightway to validate Name
-    if (!fieldRequired(student.name)) {
-        parent.find('#Namelb').html("Name is required"); //document.getElementById("Namelb").innerHTML = "Name is required";
+    if (!fieldRequired(student.Name)) {
+        parent.find('#Namelb').html("Name is required").addClass('error text-danger'); //document.getElementById("Namelb").innerHTML = "Name is required";
         check = false;
-    } else if (!isAplhabet(student.name)) {
-        parent.find('#Namelb').html("Name cannot be Number"); // document.getElementById("Namelb").innerHTML = "Name cannot be Number";
+    } else if (!isAplhabet(student.Name)) {
+        parent.find('#Namelb').html("Name cannot be Number").addClass('error text-danger'); // document.getElementById("Namelb").innerHTML = "Name cannot be Number";
         check = false;
     } else {
         parent.find('#Namelb').html(""); //document.getElementById("Namelb").innerHTML = "";
     }
     //straightway to validate Email
-    if (!fieldRequired(student.email)) {
-        parent.find('#emaillb').html("Email is required"); //document.getElementById("emaillb").innerHTML = "Email is required";
+    if (!fieldRequired(student.Email)) {
+        parent.find('#emaillb').html("Email is required").addClass('error text-danger'); //document.getElementById("emaillb").innerHTML = "Email is required";
         check = false;
-    } else if (!isEmail(student.email)) {
-        parent.find('#emaillb').html("Email is incorrect"); //document.getElementById("emaillb").innerHTML = "Email is incorrect";
+    } else if (!isEmail(student.Email)) {
+        parent.find('#emaillb').html("Email is incorrect").addClass('error text-danger'); //document.getElementById("emaillb").innerHTML = "Email is incorrect";
         check = false;
     } else {
         parent.find('#emaillb').html(""); // document.getElementById("emaillb").innerHTML = "";
     }
     //straightway to validate passwords
-    if (!fieldRequired(student.password)) {
-        parent.find('#Passwordlb').html("password is required"); //document.getElementById("Passwordlb").innerHTML = "password is required";
+    if (!fieldRequired(student.Password)) {
+        parent.find('#Passwordlb').html("password is required").addClass('error text-danger'); //document.getElementById("Passwordlb").innerHTML = "password is required";
         check = false;
     } else {
         parent.find('#Passwordlb').html(""); //document.getElementById("Passwordlb").innerHTML = "";
@@ -292,10 +337,15 @@ debugger;
     return check;
 }
 
+function GetStudentListApi(){
+    var arr=[];
+  
+  return studentArray;
+}
 
 //           End of single page crud Operation
 
-function addStudent(isruntime) {
+function AddStudent(isruntime) {
   
     var name = $('#Name').val(); //document.getElementById("Name").value;
     var fName = $('#FName').val(); // document.getElementById("FName").value;
@@ -374,40 +424,41 @@ function validateStudentForm(name, fname, email, phone, password, confirmPasswor
     //straightway to validate Name
     if (!fieldRequired(name)) {
         $('#Namelb').html("Name is required"); //document.getElementById("Namelb").innerHTML = "Name is required";
+        $('#Namelb').addClass('error text-danger');
         check = false;
     } else if (!isAplhabet(name)) {
-        $('#Namelb').val("Name cannot be Number"); // document.getElementById("Namelb").innerHTML = "Name cannot be Number";
+        $('#Namelb').val("Name cannot be Number").addClass('error text-danger'); // document.getElementById("Namelb").innerHTML = "Name cannot be Number";
         check = false;
     } else {
         $('#Namelb').html(""); //document.getElementById("Namelb").innerHTML = "";
     }
     //straightway to validate Email
     if (!fieldRequired(email)) {
-        $('#emaillb').html("Email is required"); //document.getElementById("emaillb").innerHTML = "Email is required";
+        $('#emaillb').html("Email is required").addClass('error text-danger'); //document.getElementById("emaillb").innerHTML = "Email is required";
         check = false;
     } else if (!isEmail(email)) {
-        $('#emaillb').html("Email is incorrect"); //document.getElementById("emaillb").innerHTML = "Email is incorrect";
+        $('#emaillb').html("Email is incorrect").addClass('error text-danger'); //document.getElementById("emaillb").innerHTML = "Email is incorrect";
         check = false;
     } else {
         $('#emaillb').html(""); // document.getElementById("emaillb").innerHTML = "";
     }
     //straightway to validate passwords
     if (!fieldRequired(password)) {
-        $('#Passwordlb').html("password is required"); //document.getElementById("Passwordlb").innerHTML = "password is required";
+        $('#Passwordlb').html("password is required").addClass('error text-danger'); //document.getElementById("Passwordlb").innerHTML = "password is required";
         check = false;
     } else {
         $('#Passwordlb').html(""); //document.getElementById("Passwordlb").innerHTML = "";
     }
 
     if (!fieldRequired(confirmPassword)) {
-        $('#ConfrimPasswordlb').html("password is required"); // document.getElementById("ConfrimPasswordlb").innerHTML = "password is required";
+        $('#ConfrimPasswordlb').html("password is required").addClass('error text-danger'); // document.getElementById("ConfrimPasswordlb").innerHTML = "password is required";
         check = false;
     } else {
         $('#ConfrimPasswordlb').html(""); //  document.getElementById("ConfrimPasswordlb").innerHTML = "";
 
     }
     if (!isEqual(password, confirmPassword)) {
-        $('#ConfrimPasswordlb').html("Password does not Match"); //  document.getElementById("ConfrimPasswordlb").innerHTML = "Password does not Match";
+        $('#ConfrimPasswordlb').html("Password does not Match").addClass('error text-danger'); //  document.getElementById("ConfrimPasswordlb").innerHTML = "Password does not Match";
         check = false;
     } else {
         $('#ConfrimPasswordlb').html(""); // document.getElementById("ConfrimPasswordlb").innerHTML = "";
