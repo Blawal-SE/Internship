a49678a44764a0d9b98fa872341e64a94806cf54
HAIER-PC\Hamza Aslam <blawal761@gmail.com>
2020-10-02 17:48:19 +0500

    Ajax Generic Crud Done

---
+++ jquery/Script/WebApiAjaxCall.js
@@ -0,0 +1,116 @@
+var arr=[];
+var bool=true;
+
+
+function ajaxCall(url,type,data){
+
+
+    $.ajax({
+        type: type,
+        async: false,  
+        url: url,
+        data:data,
+        //contentType: 'application/json; charset=UTF-8',  //send type of data to sever
+       // traditional: true,
+        
+        success: function (response, statusText, xhr) {
+            if(type.toLowerCase()=='get'){
+            arr=JSON.parse(JSON.stringify(response));
+        }
+           
+
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {                       
+            if(type.toLowerCase()!='get'){  bool=false;}
+
+        }
+     });
+     if(type.toLowerCase()=='get'){return arr;}
+        
+    if(type.toLowerCase()=='put'){ return bool;}
+    if(type.toLowerCase()=='post'){ return bool;}
+    if(type.toLowerCase()=='delete'){ return bool;}
+
+    
+
+// if(type.toLowerCase()=='post')
+// {
+//     $.ajax({
+//         type: type,
+//         async: false,  
+//         url: url,
+//         //contentType: 'application/json; charset=UTF-8',  //send type of data to sever
+//        // traditional: true,
+//         data:data,//{
+//         //     Id:data.Id,
+//         //     Name: data.Name,
+//         //     Fname: data.FName,
+//         //     Email: data.Email,
+//         //     Phone: data.Phone,
+//         //     Dob: data.Dob,
+//         //     Password: data.Password,
+//         //     ConfirmPassword: data.ConfirmPassword
+
+//         // },
+//         success: function (response, statusText, xhr) {
+          
+
+//         },
+//         error: function (XMLHttpRequest, textStatus, errorThrown) {                       
+//             bool=false;
+
+//         }
+//      }); 
+//      return bool;
+// }
+// if(type.toLowerCase()=='put')
+// {
+    
+//     $.ajax({
+//         type: type,
+//         async: false,  
+//         url: url,
+//         data:{
+//             Id:data.Id,
+//             Name: data.Name,
+//             Fname: data.FName,
+//             Email: data.Email,
+//             Phone: data.Phone,
+//             Dob: data.Dob,
+//             Password: data.Password,
+//             ConfirmPassword: data.ConfirmPassword
+
+//         },
+//         success: function (response, statusText, xhr) {
+          
+
+//         },
+//         error: function (XMLHttpRequest, textStatus, errorThrown) {                       
+//             bool=false;
+
+//         }
+//      });
+//      return bool;
+// }
+// if(type.toLowerCase()=='delete')
+// {
+//      $.ajax({
+//     type: type,
+//     async: false,  
+//     url: url,
+//     //contentType: 'application/json; charset=UTF-8',  //send type of data to sever
+//    // traditional: true,
+   
+//     success: function (response, statusText, xhr) {
+//     },
+//     error: function (XMLHttpRequest, textStatus, errorThrown) {                       
+//         bool=false;
+
+//     }
+//  });
+//  return bool;
+    
+// }
+
+  
+}
