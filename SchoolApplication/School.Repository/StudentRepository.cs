90d567d9b8c2d62e7e58224ed4fbd36d4534364e
HAIER-PC\Hamza Aslam <blawal761@gmail.com>
2020-10-06 17:53:43 +0500

    Crud Completed through Repository Pattern

--- SchoolApplication/School.Repository/StudentRepository.cs
+++ SchoolApplication/School.Repository/StudentRepository.cs
@@ -14,94 +14,34 @@ namespace School.Repository
     {
         #region student Repository
         public List<StudentDtoPost> GetStudents()
-        {// BaseRepository<Student> b = new BaseRepository<Student>();
-         //  Add(obj);
+        {
             List<StudentDtoPost> DtoList = new List<StudentDtoPost>();
             try
             {
                 using (SchoolContext db = new SchoolContext())
-                {
-                    var StdList=db.Students.ToList();
-                    foreach (var student in StdList)
-                    {
-                        StudentDtoPost dto = new StudentDtoPost();
-                        dto.student = student;
-                        dto.CoursesCount = db.StudentCourses.Where(x => x.StudentId == student.Id).Count();
+                {            //Simple Join to gett Student With count of Courses
+                    var students = (from s in db.Students
+                                    select new StudentDtoPost()
+                                    {
+                                        student = s,
+                                        CoursesCount = (from sc in db.StudentCourses.Where(x => x.StudentId == s.Id)
+                                                        select sc.StudentId).Count()
+                                    }).ToList();
+                    return students;
+                    //used before to get Coursecount without Jooin
+                    //var StdList = db.Students.ToList();
 
-                        DtoList.Add(dto);
-                        
-                    }
-                  
+                    //foreach (var student in StdList)
+                    //{
+                    //  StudentDtoPost dto = new StudentDtoPost();
+                    //   dto.student = student;
+                    //    dto.CoursesCount = db.StudentCourses.Where(x => x.StudentId == student.Id).Count();
 
-                }
-            }
-            catch (Exception e)
-            {
+                    //    DtoList.Add(dto);
 
-              
-            }
-
-            return DtoList;
-
-        }
-        public bool AddStudent(Student obj,String courses)
-        {// BaseRepository<Student> b = new BaseRepository<Student>();
-          //  Add(obj);
-            
-                try
-                {
-                    using (SchoolContext db = new SchoolContext())
-                    {
-                        StudentCourse stdCourse = new StudentCourse();
-                    // var Std = db.Students.OrderByDescending(x => x.Id).FirstOrDefault();
-                        db.Students.Add(obj);
-                        stdCourse.StudentId = obj.Id;
-                        var courseList = courses.Split(',').ToList();
-                        foreach (var course in courseList)
-                        {
-                            stdCourse.CourseId = Convert.ToInt32(course);
-                            db.StudentCourses.Add(stdCourse);
-                            db.SaveChanges();
-                        }
-                   
-                    }
-                }
-                catch (Exception e)
-                {
-
-                    return false;
-                }
-
-            return true;
-
-        }
-        //public bool DeleteStudent(int id)
-        //{
-        //    try
-        //    {
-        //        using (var context = new SchoolContext())
-        //        {
-        //            context.Students.Remove(context.Students.Where(x => x.Id == id).FirstOrDefault());
-        //            context.SaveChanges();
-        //            return true;
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-
-        //        return false;
-        //    }
+                    // }
 
 
-        //}
-        public Student FindStudent(int id)
-        {
-            try
-            {
-                using (var context = new SchoolContext())
-                {
-                    var student = context.Students.Where(x => x.Id == id).FirstOrDefault();
-                    return student;
                 }
             }
             catch (Exception e)
@@ -109,28 +49,142 @@ namespace School.Repository
 
                 throw e;
             }
+        
 
 
         }
-        //public void SaveStudent(Student obj)
-        //{
-        //    try
-        //    {
-        //        using (var context = new SchoolContext())
-        //        {
-        //            context.Students.Add(obj);
-        //            context.SaveChanges();
+        public StudentDtoPost FindStudent(int id)
+        {
+            try
+            {
+                
+                using (var context = new SchoolContext())
+                {
+                   var students = (from s in context.Students.Where(x => x.Id == id)
+                                    select new StudentDtoPost
+                                    {
+                                        student = s,
+                                        StudentCourses=(from sc in context.StudentCourses
+                                                        join c in context.Courses on sc.CourseId equals c.CourseId
+                                                        where sc.StudentId==id
+                                                        select new CourseDTO()
+                                                        {
+                                                            CourseId=sc.CourseId,
+                                                            Name=c.Name
+                                                        }
+                                                        ).ToList()
+                                    }).FirstOrDefault();
+                    return students;
+                }
+            }
+            catch (Exception e)
+            {
 
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
+                throw e;
+            }
+        }
+        public bool AddStudent(Student obj, String courses)
+        {
+            try
+            {
+                using (SchoolContext db = new SchoolContext())
+                {
+                    StudentCourse stdCourse = new StudentCourse();
+                    db.Students.Add(obj);
+                    stdCourse.StudentId = obj.Id;
+                    var courseList = courses.Split(',').ToList();
+                    foreach (var course in courseList)
+                    {
+                        stdCourse.CourseId = Convert.ToInt32(course);
+                        db.StudentCourses.Add(stdCourse);
+                        db.SaveChanges();
+                    }
+                     
+                }
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+
+            return true;
+
+        }
+        public bool UpdateStudent(StudentDtoPost stdPost)
+        {
+            try
+            {
+                using (SchoolContext db = new SchoolContext())
+                {
+
+                    //update first Student
+                    db.Entry(stdPost.student).State = EntityState.Modified;
+                    //now First Find Courses And delete then Add Newly come Added Courses
+                    var stdPreCourses = db.StudentCourses.Where(x => x.StudentId == stdPost.student.Id).ToList();
+                    if (stdPreCourses != null)
+                    {
+                        db.StudentCourses.RemoveRange(stdPreCourses);
+                        db.SaveChanges();
+                    }
+                    List<StudentCourse> courseList = new List<StudentCourse>();
+                    if (stdPost.Courses != null)
+                    {
+                        foreach (var Course in stdPost.Courses)
+                        {
+                            StudentCourse course_Obj = new StudentCourse();
+                            course_Obj.StudentId = stdPost.student.Id;
+                            course_Obj.CourseId = Convert.ToInt32(Course);
+                            courseList.Add(course_Obj);
+
+                        }
+                        db.StudentCourses.AddRange(courseList);
+                        db.SaveChanges();
+
+                    }
 
 
-        //    }
+                }
+                //Adding updated  Courses
+
+            
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+        public bool DeleteStudent(int id)
+        {
+            try
+            {
+                using (var context = new SchoolContext())
+                {
+                   var std= context.Students.Where(x => x.Id == id).FirstOrDefault();
+                    //  StudentCourse stdCourse = new StudentCourse();
+                    // stdCourse.StudentId = std.Id;
+                    var stdcourses = context.StudentCourses.Where(x => x.StudentId == id);
+                    foreach (var course in stdcourses)
+                    {
+                        context.StudentCourses.Remove(course);
+                        context.SaveChanges();
+                    }
+                    context.Students.Remove(std);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
 
 
-        //}
+        }
 
         #endregion
 
--- jquery/Script/StudentManagerJquery.js
+++ jquery/Script/StudentManagerJquery.js
@@ -4,20 +4,16 @@ var baseUrl = 'https://localhost:44330/api/';
 //#region Generic Ajax  crud operation On student Table 
 
 function StudentListGeneric() {
-    debugger;
-    // var stdList =GetStudentListApi;//JSON.parse(localStorage.getItem("student"));
-    // studentArray=
     serverCall(baseUrl+"Student", "get", null, successg);
-
     // ajax start to get all student and put records in Table
     function successg(result) {
         var appendRow = "";
         result.forEach(function (value, index) {
-
+     
             appendRow += "<tr><td>" + value.student.Name + "</td>" + "<td>" + value.student.FName + "</td>" + "<td>" + value.student.Email + "</td>" + "<td>" + value.student.Phone + "</td>" + "<td>" + value.student.Dob + "</td>" + "<td>" + value.student.Password + "</td>"+"<td>"+value.CoursesCount+"</td>";
-            appendRow += "<td><button class='editBtn btn btn-primary'  data-id=" + value.Id + ">Edit</button>";
-            appendRow += "<button style='margin-left:5px;' class='editNewWindowBtn btn btn-secondary'  data-id=" + value.Id + ">Edit With Courses</button>";
-            appendRow += "<button style='margin-left:5px;' class='deleteBtn btn btn-danger'  data-id=" + value.Id + ">Delete</a></td></tr>";
+            appendRow += "<td><button class='editBtn btn btn-primary'  data-id=" + value.student.Id + ">Edit</button>";
+            appendRow += "<button style='margin-left:5px;' class='editNewWindowBtn btn btn-secondary'  data-id=" + value.student.Id + ">Edit With Courses</button>";
+            appendRow += "<button style='margin-left:5px;' class='deleteBtn btn btn-danger'  data-id=" + value.student.Id + ">Delete</a></td></tr>";
             //tBody.innerHTML += appendRow;
 
         });
@@ -35,28 +31,22 @@ function StudentListGeneric() {
             function successd() {
                 StudentListGeneric();
             }
-            // deleteStudent(id);
         });
         $('.editBtn').click(function () {
-            debugger;
             var id = parseInt($(this).attr('data-id'));
             var url = baseUrl + 'Student?id=' + id;
             var parent = $(this).closest('tr');
-
-            // var student=ajaxCall(url,"get");
             serverCall(url, "get", null, successGetStudent);
-
-            function successGetStudent(student) {
-                var row = "<td>  <input id='Name' type='text'  value='" + student.Name + "'/></td>";
-                row += "<td>  <input id='FName' type='text'  value='" + student.FName + "'/></td>";
-                row += "<td>  <input id='email' type='text'  value='" + student.Email + "'/></td>";
-                row += "<td>  <input id='phone' type='text'  value='" + student.Phone + "'/></td>";
-                row += "<td>  <input id='DOB' type='date' value='" + student.Dob + "'/></td>";
-                row += "<td>  <input id='Password' type='password' value='" + student.Password + "'/></td>";
-
+            function successGetStudent(data) {
+                var row = "<td>  <input id='Name' type='text'  value='" + data.student.Name + "'/></td>";
+                row += "<td>  <input id='FName' type='text'  value='" + data.student.FName + "'/></td>";
+                row += "<td>  <input id='email' type='text'  value='" + data.student.Email + "'/></td>";
+                row += "<td>  <input id='phone' type='text'  value='" + data.student.Phone + "'/></td>";
+                row += "<td>  <input id='DOB' type='date' value='" + data.student.Dob + "'/></td>";
+                row += "<td>  <input id='Password' type='password' value='" + data.student.Password + "'/></td>";
+                row += "<td></td>";
                 row += "<td><button id='updateBtn' type='button'>Update</button><button id='updateCancelBtn' type='button'>Cancel</button>  </td>";
                 parent.html('').append(row);
-
                 var row2 = "<tr><td> <label id='Namelb'/></td>";
                 row2 += "<td><td>";
                 row2 += "<td><label id='emaillb'></label></td>";
@@ -66,12 +56,9 @@ function StudentListGeneric() {
                 row2 += "<td></td>";
                 $(row2).insertAfter(parent);
             }
-
-
             $('#updateBtn').click(function () {
-
                 var parent = $(this).closest('tr');
-                var student = {};//getStudent(id);
+                var student = {};
                 student.Id = id;
                 student.Name = parent.find('#Name').val();
                 student.FName = parent.find('#FName').val();
@@ -80,7 +67,6 @@ function StudentListGeneric() {
                 student.Dob = parent.find('#DOB').val();
                 student.Password = parent.find('#Password').val();
                 student.ConfirmPassword = parent.find('#Password').val();
-
                 if (validateStudent(student, parent.next())) {
                     serverCall(baseUrl+'Student', "put", student, successPutStudent);
                     function successPutStudent(result) {
@@ -96,30 +82,20 @@ function StudentListGeneric() {
 
 
     }
-
-
-
-
     //ajax end 
-
-
-    // var tBody = $('#stdList').val();//document.getElementById("stdList");
-
-
-
 }
 
 //add new  row 
 
 function addNewStudentRow() {
     debugger;
-    var row = "<tr><td>  <input id='Name' type='text' name='Name' /></td>";
-    row += "<td>  <input id='FName' type='text' name='FName' /></td>";
-    row += "<td>  <input id='email' type='text' name='Name' /></td>";
-    row += "<td>  <input id='phone' type='text' name='Name' /></td>";
-    row += "<td>  <input id='DOB' type='date' name='Name' /></td>";
-    row += "<td>  <input id='Password' type='password' name='Name' /></td>";
-
+    var row = "<tr><td><input id='Name' type='text' name='Name' /></td>";
+    row += "<td><input id='FName' type='text' name='FName' /></td>";
+    row += "<td><input id='email' type='text' name='Name' /></td>";
+    row += "<td><input id='phone' type='text' name='Name' /></td>";
+    row += "<td><input id='DOB' type='date' name='Name' /></td>";
+    row += "<td><input id='Password' type='password' name='Name' /></td>";
+    row += "<td></td>";
     row += "<td><button id='saveBtn' type='button'>Save</button><button id='removeBtn' type='button'>Remove</button>  </td></tr>";
     $('#stdList').append(row);
     var row2 = "<tr><td><label id='Namelb'/></label></td>";
@@ -128,6 +104,7 @@ function addNewStudentRow() {
     row2 += "<td></td>";
     row2 += "<td></td>";
     row2 += "<td><label id='Passwordlb'></label></td>";
+    row2 += "<td></td>";
     row2 += "<td></td>";
 
     $('#stdList').append(row2);
@@ -143,12 +120,12 @@ function addNewStudentRow() {
         student.Password = parent.find('#Password').val();
         student.ConfirmPassword = parent.find('#Password').val();
         if (validateStudent(student, parent.next())) {
-            if (serverCall(baseUrl+'Student', "post", student, successP, errorP)) { StudentListGeneric(); } else { alert("ajax failed to post data"); }
+            serverCall(baseUrl+'Student', "post", student, successP, errorP)
             function successP(result) {
-                return true;
+                StudentListGeneric();
             }
             function errorP(result) {
-                return false;
+                alert("ajax failed to post data");
             }
             // addStudentDynamic(student);
         }
@@ -165,8 +142,6 @@ function addNewStudentRow() {
 //#region validate Student
 
 function validateStudent(student, parent) {
-
-
     var check = true;
     //straightway to validate Name
     if (!fieldRequired(student.Name)) {
@@ -237,11 +212,9 @@ function validateStudentForm(student, parent) {
 
     return check;
 }
-
-
 //#endregion
 
-//add student Form post method
+//add student Form post method on Table
 function addStudent() {
     debugger;
     var name = $('#Name').val(); //document.getElementById("Name").value;
@@ -273,7 +246,7 @@ function addStudent() {
         "Password": password,
         "ConfirmPassword": confirmPassword 
               },
-         "Courses":courses
+         "courses":courses
        
     }
     if (validateStudentForm(student, $('#parent'))) {
@@ -322,55 +295,63 @@ function editStudentGet() {
     serverCall(baseUrl+'Student?id='+studentId,"GET",null,successg);
     function successg(data,status){
         debugger;
-        $('#id').val(data.Id);
-        $('#Name').val(data.Name);
-        $('#FName').val(data.FName);
-        $('#email').val(data.Email);
-        $('#phone').val(data.Phone);
-        $('#Password').val(data.Password);
-        $('#ConfrimPassword').val(data.ConfirmPassword);
-        $('#DOB').val(data.Dob);
-        $('#courseslct').val(data.Courses);
+        $('#id').val(data.student.Id);
+        $('#Name').val(data.student.Name);
+        $('#FName').val(data.student.FName);
+        $('#email').val(data.student.Email);
+        $('#phone').val(data.student.Phone);
+        $('#Password').val(data.student.Password);
+        $('#ConfrimPassword').val(data.student.ConfirmPassword);
+        $('#DOB').val(data.student.Dob);
+        debugger;
+        data.StudentCourses.forEach(function(value,index){
+            $('.select2').each(function () {
+                var option = $(this);
+                debugger;
+                if (option.val() == value.CourseId) {
+                    $(this).prop('selected', true);
+                }
+            });
+        });
+       localStorage.removeItem('StudentId');
     }
    
 }
 
 function editStudentPost() {
-    
-    var id = document.getElementById("id").value;
-    var name = document.getElementById("Name").value;
-    var fName = document.getElementById("FName").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var password = document.getElementById("Password").value;
-    var confirmPassword = document.getElementById("ConfrimPassword").value;
-    var dob = document.getElementById("DOB").value;
-
-    if (validateStudentForm(name, fName, email, phone, password, confirmPassword)) {
+   
+    var id = $("#id").val();
+    var name = $("#Name").val();
+    var fName = $("#FName").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    var password = $("#Password").val();
+    debugger;
+    var confirmPassword = $("#ConfrimPassword").val();
+    var dob = $("#DOB").val();
+    var coursesArr=$('#courseslct').val();
+    var student = {
+        "student":{
+        "Id": id,
+        "Name": name,
+        "FName": fName,
+        "Email": email,
+        "Phone": phone,
+        "Dob": dob.toString(),
+        "Password": password,
+        "ConfirmPassword": confirmPassword
+         },
+        "Courses":coursesArr
+    }
+    if (validateStudentForm(student,$('#parent'))) {
 
         // var stdArr = [];
         //var stdDobStr = stdDob.toString();
-        var student = {
-            "Id": id,
-            "name": name,
-            "fName": fName,
-            "email": email,
-            "Phone": phone,
-            "dob": dob.toString(),
-            "password": password,
-            "ConfrimPassword": confirmPassword
+      
+        serverCall(baseUrl+'Student?student='+JSON.stringify(student),"put",null,successP);
+        function successP(){
+         window.location.href='StudentList.html';
         }
-        var students = JSON.parse(localStorage.getItem("student"));
-
-        students.forEach(function(value, index) {
-            if (value.Id == student.Id) {
-               
-                students[index] = student;
-                localStorage.setItem("student", JSON.stringify(students));
-                window.location.href = 'C:\Users\Administrator\Desktop\Internship\StudentList.html';
-            }
-        })
-
     } else {
         return false;
     }
